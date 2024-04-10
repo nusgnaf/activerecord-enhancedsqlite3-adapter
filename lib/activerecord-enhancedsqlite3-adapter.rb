6d11db39b9f977de0e94ad107ebe3b86bc00f956
Stephen Margheim <stephen.margheim@gmail.com>
2024-04-10 22:33:20 +0200

    Setup an isolate_connection_pools config option

--- lib/activerecord-enhancedsqlite3-adapter.rb
+++ lib/activerecord-enhancedsqlite3-adapter.rb
@@ -1,3 +1,15 @@
 require "active_record"
 require "enhanced_sqlite3/version"
 require "enhanced_sqlite3/railtie"
+
+module EnhancedSQLite3
+  Error = Class.new(StandardError)
+
+  mattr_writer :isolate_connection_pools
+
+  class << self
+    def isolate_connection_pools?
+      @isolate_connection_pools ||= @@isolate_connection_pools || false
+    end
+  end
+end
